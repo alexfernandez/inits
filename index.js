2b77adf74b1bb51dc36f382c4a98be5751b785ed
Alex Fernández <alex.fernandez@mediasmart.es>
2015-04-11 14:25:28 +0200

    Skeleton files.

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Main interface for inits.
+ * (C) 2015 Alex Fernández.
+ */
+
+
+// requires
+require('prototypes');
+var InitSystem = require('./lib/initSystem.js');
+
+
+module.exports = new InitSystem();
+
